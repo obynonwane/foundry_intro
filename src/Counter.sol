5f705e3932215ce9a4caae92f3392fc59a10bb92
obynonwane <biostechnologyng@gmail.com>
2025-02-23 21:03:42 +0100

    added a youtube link

--- src/Counter.sol
+++ src/Counter.sol
@@ -24,3 +24,4 @@ contract Counter {
 // Second script: first_foundry_proj % forge script script/Counter.s.sol:CounterScript --fork-url http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
 // Make cast call: cast call 0x5FbDB2315678afecb367f032d93F642f64180aa3 "getCount()(int)"
 // Execute transaction: cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "getCount()(int)" --private-key $PRIVATE_KEY
+// https://www.youtube.com/watch?v=uelA2U9TbgM
